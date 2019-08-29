d92ce0778494de7d1333a113fc85fb6c5504b062
JG Carvalho <jg@jgcarv.com>
2019-08-29 17:21:43 -0300

    WIP first implmentation

---
+++ contracts/Tests Cases/UR+ERC777/ERC777Reciever.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.5.10;
+
+import "../../UniversalReciever/UniversalReciever.sol";
+import "../../Account/Account.sol";
+
+
+contract ERC777Reciever is UniversalReciever{
+
+    event RecievedERC777(address _operator, address _from, address _to, uint256 _amount);
+
+    bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH =
+        0xb281fc8c12954d22544db45de3159a39272895b169a852b314f9cc762e44c53b;
+
+    function toERC777Data(bytes memory _bytes) internal pure returns(address _operator, address _from, address _to, uint256 _amount) {
+        require(_bytes.length == 72, "data has wrong size");
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            _operator := mload(add(add(_bytes, 0x14), 0x0))
+            _from := mload(add(add(_bytes, 0x14), 0x14))
+            _to := mload(add(add(_bytes, 0x28), 0x28))
+            _amount := mload(add(add(_bytes, 0x20), 0x42))
+
+        }
+    }
+
+    function recieve(address sender,bytes32 typeId,bytes calldata data) external{
+        require(typeId == TOKENS_RECIPIENT_INTERFACE_HASH);
+        (address _operator, address _from, address _to, uint256 _amount) = toERC777Data(data);
+        emit RecievedERC777(_operator, _from, _to, _amount);
+    }
+}
---
+++ contracts/Tests Cases/UR+ERC777/RecievingAccount.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.5.10;
+
+import "./ERC777Reciever.sol";
+import "../../UniversalReciever/UniversalReciever.sol";
+import "../../Account/Account.sol";
+
+contract RecievingAccount is Account, UniversalReciever {
+    
+    ERC777Reciever public reciever;
+
+    function changeReciever(address _newReciever) onlyOwner external {
+        reciever = ERC777Reciever(_newReciever);
+    }
+
+    function recieve(address sender,bytes32 typeId ,bytes calldata data) external{
+        reciever.recieve(sender, typeId,data);
+        emit Received(sender, typeId,data);
+    }
+
+
+}
