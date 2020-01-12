3ed570cd9db1d6d9efba077e8a90594a644087b8
Fabian Vogelsteller <fabian@frozeman.de>
2020-01-12 16:44:01 +0100

    made msg.sender owner

--- contracts/Tests Scenarios/DigitalCertifcate Gen1/DigitalCertificte-gen1.sol
+++ contracts/Tests Scenarios/DigitalCertifcate Gen1/DigitalCertificte-gen1.sol
@@ -3,7 +3,7 @@ import "https://github.com/lukso-network/standards-scenarios/blob/digital-certif
 contract DCGen1 is DigitalCertificate {
 
     constructor(string memory name, string memory symbol, address[] memory defaultOperators) ERC777(name, symbol, defaultOperators) public {
-
+        owner = msg.sender;
     }
 
     // certificate should not be able to receive ETH/LYX
@@ -12,29 +12,6 @@ contract DCGen1 is DigitalCertificate {
 
     // TODO add execute?
 
-    /**
-     * @dev See `IERC20.transfer`.
-     *
-     * Overwritten transfer, allows `owner` of the DigitalCertificate, to transfer on behalf of
-     *
-     * Unlike `send`, `recipient` is _not_ required to implement the `tokensReceived`
-     * interface if it is a contract.
-     *
-     * Also emits a `Sent` event.
-     */
-    function transfer(address recipient, uint256 amount) external returns (bool) {
-        require(recipient != address(0), "ERC777: transfer to the zero address");
-
-        address from = msg.sender;
-
-        super._callTokensToSend(from, from, recipient, amount, "", "");
-
-        super._move(from, from, recipient, amount, "", "");
-
-        super._callTokensReceived(from, from, recipient, amount, "", "", false);
-
-        return true;
-    }
 
     /**
      * @dev Creates `amount` tokens and assigns them to `account`, increasing
