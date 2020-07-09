80506e17a860fb5c03c521c8bb4f58b5fc81729a
Fabian Vogelsteller <fabian@frozeman.de>
2020-07-09 18:48:27 +0200

    added account 1820 tests

--- test/erc777_1820.js
+++ test/erc777_1820.js
@@ -1,16 +1,23 @@
-const ERC777UniversalReceiver_1820 = artifacts.require("ERC777UniversalReceiver");
+const ERC777UniversalReceiver_1820 = artifacts.require("ERC777UniversalReceiver_1820");
 const Account = artifacts.require("Account");
 const PlainERC725Account = artifacts.require("ERC725Account");
 const ExternalERC777UniversalReceiverTester = artifacts.require("ExternalERC777UniversalReceiverTester");
 const ExternalERC777UniversalReceiverRejectTester = artifacts.require("ExternalERC777UniversalReceiverRejectTester");
 
-const {expectRevert} = require("openzeppelin-test-helpers");
+const {expectRevert, singletons} = require("openzeppelin-test-helpers");
 
 // Get key: keccak256('LSP1UniversalReceiverAddress')
 const UNIVERSALRECEIVER_KEY = '0x8619f233d8fc26a7c358f9fc6d265add217d07469cf233a61fc2da9f9c4a3205';
+const TOKENS_RECIPIENT_INTERFACE_HASH = "0x2352f13a810c120f366f70972476f743e16a9f2196b4b60037b84185ecde66d3"; // keccak256("LSP1_ERC777TokensRecipient")
+const OPERATION_CALL = 0;
 
-contract("ERC777 without 1820", accounts => {
-    context("Using no-1820 ERC777", async () => {
+contract("ERC777", accounts => {
+    let erc1820;
+    beforeEach(async function () {
+        erc1820 = await singletons.ERC1820Registry(accounts[1]);
+    });
+
+    context("Using 1820 ERC777", async () => {
         const owner = accounts[9];
 
         let account,
@@ -27,6 +34,10 @@ contract("ERC777 without 1820", accounts => {
             erc777 = await ERC777UniversalReceiver_1820.new("MyToken", "TKN", [accounts[0]]);
             await erc777.mint('100000000', {from: accounts[0]});
             account = await Account.new(owner, {from: owner});
+
+            // set ERC1820 implementer
+            const callData = erc1820.contract.methods.setInterfaceImplementer(account.address, TOKENS_RECIPIENT_INTERFACE_HASH, account.address).encodeABI();
+            account.execute(OPERATION_CALL, erc1820.address, 0, callData, {from: owner});
         });
 
         it("Transfer correctly between regular addresses", async () => {
@@ -94,7 +105,7 @@ contract("ERC777 without 1820", accounts => {
         });
 
         it("Forcefully send regardless of interface (using ERC20 transfer)", async () => {
-            const receiver = await Account.new(accounts[0]);
+            const receiver = await PlainERC725Account.new(accounts[0]);
             let initBal = await erc777.balanceOf(receiver.address);
             let tx = await erc777.transfer(receiver.address, 500);
             console.log(
